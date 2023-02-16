947a5d4c855815a46c9a5865dba44cb2251ba8ee
SoloviovO <soloviov.o.m@gmail.com>
2023-02-16 22:59:06 +0200

    done without css

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+
+export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <div>
+      {options.map(option => {
+        return (
+          <button
+            type="button"
+            onClick={() => onLeaveFeedback(option)}
+            key={option}
+          >
+            {option}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.string),
+  onLeaveFeedback: PropTypes.func,
+};
